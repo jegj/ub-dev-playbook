81df94b8bc2ab84ad9ad391c1fa62be6dd163c49
Javier Galarza <jegj57@gmail.com>
2024-07-04 07:52:06 -0500

    chore: add surronding to nvim config

--- files/nvim/config/lazy.lua
+++ files/nvim/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.aerial" },
 		{ import = "lazyvim.plugins.extras.lang.go" },
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
+		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		-- { import = "lazyvim.plugins.extras.ui.edgy" },
 		-- { import = "lazyvim.plugins.extras.formatting.prettier" },
 		-- import/override with your plugins
