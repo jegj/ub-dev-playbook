662570e4ab9ba0ab098cc7c97dbfd5b9023ca512
Javier Galarza <jegj57@gmail.com>
2025-06-28 06:52:17 -0500

    feat: disable codeium

--- files/nvim/config/lazy.lua
+++ files/nvim/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.ai.copilot" },
 		{ import = "lazyvim.plugins.extras.editor.aerial" },
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
-		{ import = "lazyvim.plugins.extras.ai.codeium" },
+		-- { import = "lazyvim.plugins.extras.ai.codeium" },
 
 		-- { import = "lazyvim.plugins.extras.ui.edgy" },
 		-- { import = "lazyvim.plugins.extras.formatting.prettier" },
