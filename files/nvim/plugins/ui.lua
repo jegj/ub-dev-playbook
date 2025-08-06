cc1347f6233703209957e51cf5acaa000ca33f6f
Javier Galarza <jegj57@gmail.com>
2025-08-06 07:30:24 -0500

    feat: remove duplicated zen mode

--- files/nvim/plugins/ui.lua
+++ files/nvim/plugins/ui.lua
@@ -10,19 +10,6 @@ return {
 		},
 	},
 
-	{
-		"folke/zen-mode.nvim",
-		cmd = "ZenMode",
-		opts = {
-			plugins = {
-				gitsigns = true,
-				tmux = true,
-				kitty = { enabled = false, font = "+2" },
-			},
-		},
-		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-	},
-
 	{ "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
 
 	{
