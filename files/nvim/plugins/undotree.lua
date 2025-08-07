a7083c631d0f680ead4a493adc3539125a23fa6d
Javier Galarza <jegj57@gmail.com>
2025-08-07 06:46:08 -0500

    feat: improve undotree setup

--- files/nvim/plugins/undotree.lua
+++ files/nvim/plugins/undotree.lua
@@ -1,10 +1,37 @@
 return {
-  "mbbill/undotree",
-  keys = {
-    {
-      "<leader><F5>",
-      "<cmd>UndotreeToggle<CR>",
-      desc = "UndotreeToggle",
-    },
-  },
+	{
+		"mbbill/undotree",
+		cmd = "UndotreeToggle",
+		keys = {
+			-- [U]ndo [T]ree
+			{ "<leader><F5>", "<cmd>UndotreeToggle<cr>", desc = "Undo Tree Toggle" },
+		},
+		init = function()
+			-- Persist undo, refer https://github.com/mbbill/undotree#usage
+			local undodir = vim.fn.expand("~/.undo-nvim")
+
+			if vim.fn.has("persistent_undo") == 1 then
+				if vim.fn.isdirectory(undodir) == 0 then
+					os.execute("mkdir -p " .. undodir)
+				end
+
+				vim.opt.undodir = undodir
+				vim.opt.undofile = true
+			end
+
+			-- set layout style to 2, let g:undotree_WindowLayout = 2
+			vim.g.undotree_WindowLayout = 2
+		end,
+	},
+	{
+		"folke/edgy.nvim",
+		optional = true,
+		opts = function(_, opts)
+			opts.left = opts.left or {}
+			table.insert(opts.left, {
+				title = "Undo Tree",
+				ft = "undotree",
+			})
+		end,
+	},
 }
