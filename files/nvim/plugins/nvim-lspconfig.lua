b43084a46e3cc592bc6d10d2d90e8323af742d37
Javier Galarza <jegj57@gmail.com>
2025-08-07 06:45:40 -0500

    feat: remove tsserver config

--- files/nvim/plugins/nvim-lspconfig.lua
+++ files/nvim/plugins/nvim-lspconfig.lua
@@ -4,34 +4,7 @@ return {
 		servers = {
 			eslint = {},
 			tsserver = {
-				root_dir = function(...)
-					return require("lspconfig.util").root_pattern(".git")(...)
-				end,
-				single_file_support = false,
-				settings = {
-					typescript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "literal",
-							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = false,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-							includeInlayEnumMemberValueHints = true,
-						},
-					},
-					javascript = {
-						inlayHints = {
-							includeInlayParameterNameHints = "all",
-							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-							includeInlayFunctionParameterTypeHints = true,
-							includeInlayVariableTypeHints = true,
-							includeInlayPropertyDeclarationTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-							includeInlayEnumMemberValueHints = true,
-						},
-					},
-				},
+				enable = false,
 			},
 		},
 		setup = {
