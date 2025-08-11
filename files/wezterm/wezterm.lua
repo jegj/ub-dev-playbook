2cf40b324b4e69b8ec66bc654217fc4a043f3658
Javier Galarza <jegj57@gmail.com>
2025-08-11 06:47:53 -0500

    feat: improve wezterm config

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -4,6 +4,12 @@ local config = wezterm.config_builder()
 config.color_scheme = "Apple Classic"
 config.font_size = 14
 
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 700
+config.window_background_opacity = 0.95
+config.window_decorations = "TITLE | RESIZE"
+config.enable_tab_bar = false
+
 --[[ 
 For windows WSL
 config.default_domain = 'WSL:Ubuntu-18.04'
