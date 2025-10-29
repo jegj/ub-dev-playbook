d0f85bb3cd775ac992c811f2af7579ce78139bce
Javier Galarza <jegj57@gmail.com>
2025-10-29 07:39:01 -0500

    fix: remove duplicated imports in lazy config file

--- files/nvim/config/lazy.lua
+++ files/nvim/config/lazy.lua
@@ -31,9 +31,6 @@ require("lazy").setup({
 		-- { import = "lazyvim.plugins.extras.formatting.biome" },
 		-- { import = "lazyvim.plugins.extras.formatting.prettier" },
 		-- { import = "lazyvim.plugins.extras.linting.eslint" },
-
-		-- { import = "lazyvim.plugins.extras.ui.edgy" },
-		-- { import = "lazyvim.plugins.extras.formatting.prettier" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
